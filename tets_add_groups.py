07d1206636436b427220241cee82857718def148
DiLysenk <46103863+DiLysenk@users.noreply.github.com>
2020-01-20 12:40:00 +0300

    Добавленны вспомогательные методы и классы

--- tets_add_groups.py
+++ tets_add_groups.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from group import Group
 
 class Test1():
 
@@ -23,40 +24,41 @@ class Test1():
         self.driver.quit()
 
     def test_1(self):
-
         self.open_home_page()
-
-        self.login()
-
+        self.login(username="admin", password="secret")
         self.open_groups_page()
-
-        self.create_group()
-
-
+        self.create_group(Group(name_group="buranzev22", header="первый", footer="второй"))
         self.open_groups_page()
+        self.logout()
 
+    def test_add_empty_group(self):
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_groups_page()
+        self.create_group(Group(name_group="", header="", footer=""))
+        self.open_groups_page()
         self.logout()
 
     def logout(self):
         self.driver.find_element(By.LINK_TEXT, "Logout").click()
 
-    def create_group(self):
+    def create_group(self, group):
         # создать группу
         self.driver.find_element(By.NAME, "new").click()
         # Заполнение форм групп
         self.driver.find_element(By.NAME, "group_name").click()
-        self.driver.find_element(By.NAME, "group_name").send_keys("buranzev22")
+        self.driver.find_element(By.NAME, "group_name").send_keys(group.name_group)
         self.driver.find_element(By.NAME, "group_header").click()
-        self.driver.find_element(By.NAME, "group_header").send_keys("первый")
-        self.driver.find_element(By.NAME, "group_footer").send_keys("второй")
+        self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
+        self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
         self.driver.find_element(By.NAME, "submit").click()
 
     def open_groups_page(self):
         self.driver.find_element(By.LINK_TEXT, "groups").click()
 
-    def login(self):
-        self.driver.find_element(By.NAME, "user").send_keys("admin")
-        self.driver.find_element(By.NAME, "pass").send_keys("secret")
+    def login(self, username, password):
+        self.driver.find_element(By.NAME, "user").send_keys(username)
+        self.driver.find_element(By.NAME, "pass").send_keys(password)
         self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
 
     def open_home_page(self):
